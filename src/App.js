70ad99bbd3579aad574f3409ffd8752c52c4f337
prashanthakur <thakurprince9910@gmail.com>
2023-02-27 00:02:40 +0530

    sw mt mb-5

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Services from "./components/Services";
 // import Form from "./components/Form";
 import ComingSoon from "./components/ComingSoon";
-import ContactUs from "./components/ContactUs";
+// import ContactUs from "./components/ContactUs";
 import Form from "./components/Form";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
